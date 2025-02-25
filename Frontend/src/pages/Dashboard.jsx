9442df0326105a19d4a0ee5079cdec3158a7ade0
Utkarsh Tiwari <153006721+utkxrsh13@users.noreply.github.com>
2025-02-26 00:24:26 +0530

    Update Dashboard.jsx

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div id='dashboard' className='ml-[20%] pl-2'>Dashboard</div>
+    <div id='dashboard' className='ml-[20%] pl-2'>Dash</div>
   )
 }
 
